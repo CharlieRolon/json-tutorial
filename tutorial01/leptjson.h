c9380fe74f23f8d7de0c5cf8a0c5411c129e64cc
CharlieR <962953995@qq.com>
2022-03-02 23:16:12 +0800

    modified

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -9,9 +9,9 @@ typedef struct {
 
 enum {
     LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_EXPECT_VALUE, /*json只含有空白*/
+    LEPT_PARSE_INVALID_VALUE, 
+    LEPT_PARSE_ROOT_NOT_SINGULAR /*json在一个值和空白之后还有其他字符*/
 };
 
 int lept_parse(lept_value* v, const char* json);
